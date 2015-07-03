3f93647c0122955e53772d14d362db999e915cd6
Caroline Yu <Yu@Carolines-MacBook-Pro.local>
2015-07-03 16:51:35 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -8,18 +8,18 @@
 
 #import "MainScene.h"
 #import "Obstacle.h"
-@interface CGPointObject : NSObject
-    {
-        CGPoint _ratio;
-        CGPoint _offset;
-        CCNode *__unsafe_unretained _child; // weak ref
-    }
-    @property (nonatomic,readwrite) CGPoint ratio;
-    @property (nonatomic,readwrite) CGPoint offset;
-    @property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
-    +(id) pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
-    -(id) initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
-    @end
+
+@interface CGPointObject : NSObject{
+    CGPoint _ratio;
+    CGPoint _offset;
+    CCNode *__unsafe_unretained _child; //weak ref
+}
+@property (nonatomic,readwrite) CGPoint ratio;
+@property (nonatomic,readwrite) CGPoint offset;
+@property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
++(id) pointwithCGPoint:(CGPoint) point offset: (CGPoint) offset;
+-(id) initwithCGPoint:(CGPoint) point offset: (CGPoint) offset;
+@end
 @implementation MainScene {
     CGPoint _cloudParallaxRatio;
     CGPoint _bushParallaxRatio;
@@ -189,6 +189,7 @@
         }
     }
     
+    
     _parallaxBackground.position = ccp(_parallaxBackground.position.x - (character.physicsBody.velocity.x * delta), _parallaxBackground.position.y);
     
     // loop the bushes
@@ -226,6 +227,29 @@
         }
     }
 
+//    
+//    //move and loop the bushes
+//    for(CCNode *bush in _bushes){
+//        //move the bush
+//        bush.position = ccp(bush.position.x - (character.physicsBody.velocity.x * delta), bush.position.y);
+//        
+//        //if the left corner is one complete width off the screen, move to right
+//        if(bush.position.x <= (-1 * bush.contentSize.width)){
+//            bush.position = ccp(bush.position.x + 2 * bush.contentSize.width, bush.position.y);
+//        }
+//    }
+//    // move and loop the clouds
+//    for (CCNode *cloud in _clouds) {
+//        // move the cloud
+//        cloud.position = ccp(cloud.position.x -(character.physicsBody.velocity.x * delta), cloud.position.y);
+//        
+//        // if the left corner is one complete width off the screen,
+//        // move it to the right
+//        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+//            cloud.position = ccp(cloud.position.x + 2 * cloud.contentSize.width, cloud.position.y);
+//        }
+//    }
+//
     
     NSMutableArray *offScreenObstacles = nil;
     
