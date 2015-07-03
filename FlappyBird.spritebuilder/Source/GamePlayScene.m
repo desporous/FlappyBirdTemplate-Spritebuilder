897173dc272bbac3971b92c02f153e6d99fcc560
Caroline Yu <Yu@Carolines-MacBook-Pro.local>
2015-07-03 13:31:51 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,11 +9,24 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
+    
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    //will run every frame
+    //delta = time since last time it was run
+    timeSinceObstacle = timeSinceObstacle + delta;
+    
+    if(timeSinceObstacle > 2.0f){
+        //add new obstacle
+        [self addObstacle];
+        
+        //resets timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 -(void)touchBegan:(CCTouch*)touch withEvent:(CCTouchEvent*)event{
